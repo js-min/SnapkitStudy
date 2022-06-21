ec20c2da44dcd449859e0a7387424ff0dcbe02ea
js-min <alswltn1@korea.ac.kr>
2022-06-21 15:21:15 +0900

    Initial Commit

---
+++ SnapkitStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapkitStudy
+//
+//  Created by JISU MIN on 2022/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
